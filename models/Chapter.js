7cd4aecd6caef8f7b547747cf1b074d048aadcc5
valencaceres <vcaceres102@gmail.com>
2023-01-02 18:59:45 -0300

    settings and chapters models

---
+++ models/Chapter.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { ObjectId } from "mongoose";
+
+const chapterSchema = new mongoose.Schema({
+    comic_id: {type: mongoose.Types.ObjectId, required: true},
+    title: {type: String, required: true},
+    pages: {type: Array, required: true},
+    order: {type: Number},
+}, {timestamps: true})
+
+export const Chapter = mongoose.model('chapters', chapterSchema)
---
+++ models/data/chapter.data.js
@@ -0,0 +1,8 @@
+import 'dotenv/config.js'
+import '../../config/database.js'
+import { Chapter } from '../Chapter.js'
+
+let chapter = [   
+]
+
+Chapter.insertMany(chapter)
