3b958f35c8f0d22ef154855587f98fb2e551743b
Candela Cañete <candela.caniete1@gmail.com>
2023-01-04 11:46:05 -0300

    schema&validation

--- routes/companies.route.js
+++ routes/companies.route.js
@@ -1,9 +1,11 @@
 import express  from "express";
 import controller from "../controllers/companies.controller.js";
+import schema from '../schemas/companies.schema.js'
+import validator from "../middlewares/validator.js";
 const { create } = controller
 let router = express.Router()
 
-router.post('/', create)
+router.post('/',validator(schema), create)
 
 
 export default router
