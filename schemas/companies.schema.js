c745111cb252125891f1f08a4d71209506442912
Candela Cañete <candela.caniete1@gmail.com>
2023-01-04 12:03:23 -0300

    schema corrections

--- schemas/companies.schema.js
+++ schemas/companies.schema.js
@@ -2,7 +2,7 @@ import joi from 'joi-oid'
 
 const schema = joi.object({
     name: joi.string().required().min(3).max(20),
-    logo: joi.string().required(),
+    logo: joi.string().uri().required().min(8),
     website: joi.string().required().min(4),
     description: joi.string().required().min(8).max(100),
     user_id: joi.objectId().required(),
