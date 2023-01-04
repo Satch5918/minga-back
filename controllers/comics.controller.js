3a1f6a0aefa678f856c83111e49420fb856d927e
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-01-03 22:15:27 -0500

    agregue en el controlador el new_comic

--- controllers/comics.controller.js
+++ controllers/comics.controller.js
@@ -5,10 +5,11 @@ const controller = {
     create: async (req, res, next) => {
         try {
             const {author_id,company_id,title,photo,description,category} = req.body
-            await Comic.create({author_id,company_id,title,photo,description,category})
+            let comic =await Comic.create({author_id,company_id,title,photo,description,category})
             res.status(201).json({
                 success: true,
-                response: 'done'
+                response: 'done',
+                new_comic: comic
             })
         } catch (error) {
             next(error)
