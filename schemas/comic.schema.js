5e7e9fc083350f03de28b63dac74f28cb695f15a
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-01-05 22:51:44 -0500

    adheri el mensaje en schema.

--- schemas/comic.schema.js
+++ schemas/comic.schema.js
@@ -1,12 +1,28 @@
 import Joi from 'joi-oid'
 
 const schema = Joi.object({
-    author_id: Joi.objectId().required(),
+    author_id: Joi.objectId().required().messages({
+        'any.required': 'AUTHOR_ID_REQUIRED',
+        'string.empty': 'AUTHOR_ID_REQUIRED'
+    }),
     company_id: Joi.objectId(),
-    title: Joi.string().min(1).required(),
-    photo: Joi.string().min(1).required(),
-    description: Joi.string().min(1).required(),
-    category: Joi.objectId().required() 
+    title: Joi.string().min(1).required().messages({
+        'any.required': 'TITLE_REQUIRED',
+        'string.empty': 'TITLE_REQUIRED'
+    }),
+    photo: Joi.string().min(1).required().uri().messages({
+        'any.required': 'IMG_REQUIRED',
+        'string.empty': 'IMG_REQUIRED',
+        'string.uri': 'INVALID_URL'
+    }),
+    description: Joi.string().min(1).required().messages({
+        'any.required': 'DESCRIPTION_REQUIRED',
+        'string.empty': 'DESCRIPTION_REQUIRED'
+    }),
+    category: Joi.objectId().required().messages({
+        'any.required': 'CATEGORY_ID_REQUIRED',
+        'string.empty': 'CATEGORY_ID_REQUIRED'
+    }) 
 })
 
 export default schema
