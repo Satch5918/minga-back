9e25b46db7c0d8a02d038f297d436558b82d1c0c
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-01-06 11:43:41 -0500

    corregi los mensajes del schema

--- schemas/comic.schema.js
+++ schemas/comic.schema.js
@@ -2,26 +2,26 @@ import Joi from 'joi-oid'
 
 const schema = Joi.object({
     author_id: Joi.objectId().required().messages({
-        'any.required': 'AUTHOR_ID_REQUIRED',
-        'string.empty': 'AUTHOR_ID_REQUIRED'
+        'any.required': 'please put author id',
+        'string.empty': 'please put author id'
     }),
     company_id: Joi.objectId(),
     title: Joi.string().min(1).required().messages({
-        'any.required': 'TITLE_REQUIRED',
-        'string.empty': 'TITLE_REQUIRED'
+        'any.required': 'please put title of manga or comic',
+        'string.empty': 'please put title of manga or comic'
     }),
     photo: Joi.string().min(1).required().uri().messages({
-        'any.required': 'IMG_REQUIRED',
-        'string.empty': 'IMG_REQUIRED',
-        'string.uri': 'INVALID_URL'
+        'any.required': 'please put an url image ',
+        'string.empty': 'please put an url image ',
+        'string.uri': 'please put an url'
     }),
     description: Joi.string().min(1).required().messages({
-        'any.required': 'DESCRIPTION_REQUIRED',
-        'string.empty': 'DESCRIPTION_REQUIRED'
+        'any.required': 'please put description of manga or comic',
+        'string.empty': 'please put description of manga or comic'
     }),
     category: Joi.objectId().required().messages({
-        'any.required': 'CATEGORY_ID_REQUIRED',
-        'string.empty': 'CATEGORY_ID_REQUIRED'
+        'any.required': 'please put category id',
+        'string.empty': 'please put category id'
     }) 
 })
 
