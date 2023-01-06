2ccc90e9d68033e20524e402fc4bc607f59b9f04
Alejandro <alexanderdacal1@gmail.com>
2023-01-06 14:54:09 -0300

    final

--- routes/author.route.js
+++ routes/author.route.js
@@ -2,6 +2,7 @@ import controller from "../controllers/author.controllers.js";
 import schema from "../schemas/authors.schema.js";
 import  validator  from "../middlewares/validator.js";
 import express from 'express'
+import authorIsActive from "../middlewares/authorIsActive.js";
 let router = express.Router();
 
 const {create} = controller;
