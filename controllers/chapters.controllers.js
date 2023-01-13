47b7359c8ccc61afaa41728c7182afdefe93a31b
Satch5918 <satch_elfante@hotmail.com>
2023-01-13 12:17:43 -0300

    nuevo controlador y arreglo de rutas para Chapters

--- controllers/chapters.controllers.js
+++ controllers/chapters.controllers.js
@@ -3,12 +3,12 @@ import {Chapter}  from '../models/Chapter.js'
 const controller = {
     create: async(req, res, next) => {
         try{
-           const { comic_id, title, pages, order } = req.body
-           await Chapter.create({comic_id, title, pages, order})
-           res.status(201).json({
+            const { comic_id, title, pages, order } = req.body
+            await Chapter.create({comic_id, title, pages, order})
+            res.status(201).json({
             success: true,
             response: 'Done'
-           })
+          })
         }
         catch(error){
             next(error)
--- routes/chapters.route.js
+++ routes/chapters.route.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import controller from '../controllers/chapters.controllers.js'
-const {create, get_pages} = controller
 import schema from '../schemas/chapters.schema.js'
 import validator from '../middlewares/validator.js'
 import orderExists from '../middlewares/orderExist.js'
+import details from '../controllers/chapter.details.controller.js'
 import { get } from 'mongoose'
+
 let router = express.Router()
+const {create, get_pages} = controller
 
 router.post('/', validator(schema), orderExists, create)
-router.get('/:_id', get_pages)  //params
+router.get('/order', details.get_comics_order )//params
+router.get('/pages/:_id', get_pages)
 /* router.get('/', get_pages) */ //query
 
 export default router; 
