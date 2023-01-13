cb5d5a840d230a42d306c2ffa2b62d2c212be73a
ElfanteCristianXVIII <79940633+Satch5918@users.noreply.github.com>
2023-01-13 12:16:12 -0300

    funcionando en postman controlador y route (#46)
    * funcionando en postman controlador y route
    
    * using query
    
    * corregido el params

--- controllers/chapters.controllers.js
+++ controllers/chapters.controllers.js
@@ -12,8 +12,21 @@ const controller = {
         }
         catch(error){
             next(error)
+        }    
+    },
+    get_pages: async (req, res, next) => {
+        const { _id } = req.params
+        try {
+          const comic = await Chapter.findById(_id)
+          console.log(comic)
+          res.status(200).json({
+            success: true,
+            response: comic
+          })
+        }catch (error) {
+          next(error)
         }
-    }
+    },
 }
 
-export default controller
+export default controller;
--- routes/chapters.route.js
+++ routes/chapters.route.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import controller from '../controllers/chapters.controllers.js'
-const {create} = controller
+const {create, get_pages} = controller
 import schema from '../schemas/chapters.schema.js'
 import validator from '../middlewares/validator.js'
 import orderExists from '../middlewares/orderExist.js'
+import { get } from 'mongoose'
 let router = express.Router()
 
 router.post('/', validator(schema), orderExists, create)
+router.get('/:_id', get_pages)  //params
+/* router.get('/', get_pages) */ //query
 
-export default router
+export default router; 
