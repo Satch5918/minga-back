e0d271c76cb0bef375c9c156c81a2ab00b243b92
valencaceres <vcaceres102@gmail.com>
2023-01-16 16:45:17 -0300

    cambios a ingles

--- controllers/chapters.five.controllers.js
+++ controllers/chapters.five.controllers.js
@@ -2,26 +2,26 @@ import { Chapter } from "../models/Chapter.js";
 
 const controllerChDetails = {
     get_comic_chapters:  async (req, res, next) => {
-        let consultas = {comic_id: req.query.comic_id}
-        let ordenamineto = {order: "asc"}
-        let paginacion ={
+        let query = {comic_id: req.query.comic_id}
+        let ordering = {order: "asc"}
+        let pagination ={
             page : 1,
             limit: 5
         }
         if(req.query.page){
-            paginacion.page = req.query.page
+            pagination.page = req.query.page
         }
         if(req.query.limit){
-            paginacion.limit = req.query.limit
+            pagination.limit = req.query.limit
         }
         if(req.query.sort){
-            ordenamineto = {order: req.query.sort}
+            ordering = {order: req.query.sort}
             }
         try{
-            const chapters = await Chapter.find(consultas, '-_id')
-            .sort(ordenamineto)
-            .skip(paginacion.page > 0 ? ((paginacion.page - 1) * paginacion.limit) : 0)
-            .limit(paginacion.limit)
+            const chapters = await Chapter.find(query, '-_id')
+            .sort(ordering)
+            .skip(pagination.page > 0 ? ((pagination.page - 1) * pagination.limit) : 0)
+            .limit(pagination.limit)
             res.status(200).json({
                 success: true,
                 response: chapters
