5f3daa8dd6ca328b147aba1701ee75e566eff36d
Candela Cañete <candela.caniete1@gmail.com>
2023-01-17 13:55:51 -0300

    final changes

--- controllers/comic.from.company.controller.js
+++ controllers/comic.from.company.controller.js
@@ -4,7 +4,7 @@ import { Comic }  from "../models/Comic.js"
 const controllerCompany = {
     get_comics_from_cia: async(req, res, next) => {
         let query = {}
-        let order = {}
+        let order = {createdAt: "asc"}
         let pagination = {
             limit: 5,
             page: 1
@@ -13,24 +13,18 @@ const controllerCompany = {
         if(req.query.category){
             query = {
               ...query, 
-              category: req.query.category
+              category: req.query.category.split(",")
             }
         }
+
         if(req.query.company_id){
             query = {
                 ... query,
                 company_id: req.query.company_id
             }
         }
-        if (req.query.page){
-            query = {
-                ...query, 
-                page: req.query.page
-            }
-        }
-        if(req.query.order){
-            order = {createdAt: req.query.order}
-        }
+        
+    
         if(req.query.page){
             pagination.page = req.query.page
         }
@@ -40,21 +34,22 @@ const controllerCompany = {
     
         try{
             let all = await Comic
-            .find(query, 'title photo description category createdAt updatedAt -_id')
+            .find(query, 'title photo description category -_id')
             .sort(order)
             .limit(pagination.limit)
             .skip(pagination.page > 0 ? ((pagination.page - 1) * pagination.limit) : 0)
             if(all){
-                res.status(200).json({
+                return res.status(200).json({
                     succes: true,
                     response: all
                 })
             }
-        }catch(error){
-            res.status(404).json({
+            return res.status(404).json({
                 succes: false,
                 response: "Not found"
             })
+        }catch(error){
+           next(error)
 
         }
     }
