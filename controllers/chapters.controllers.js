0964d6819c8d906f64b16ccd12e2d165a1b3100e
ElfanteCristianXVIII <79940633+Satch5918@users.noreply.github.com>
2023-01-17 10:59:36 -0300

    READY to MERGE (#53)
    * Armando Controlador y ruta
    
    * funcionando en postman controlador y route (#46)
    
    * funcionando en postman controlador y route
    
    * using query
    
    * corregido el params
    
    * Sub chapter 2 (#52)
    
    * funcionando en postman controlador y route
    
    * using query
    
    * corregido el params
    
    * nuevo controlador y arreglo de rutas para Chapters
    
    * BACK TERMINADO

--- controllers/chapters.controllers.js
+++ controllers/chapters.controllers.js
@@ -3,17 +3,30 @@ import {Chapter}  from '../models/Chapter.js'
 const controller = {
     create: async(req, res, next) => {
         try{
-           const { comic_id, title, pages, order } = req.body
-           await Chapter.create({comic_id, title, pages, order})
-           res.status(201).json({
+            const { comic_id, title, pages, order } = req.body
+            await Chapter.create({comic_id, title, pages, order})
+            res.status(201).json({
             success: true,
             response: 'Done'
-           })
+          })
         }
         catch(error){
             next(error)
+        }    
+    },
+    get_pages: async (req, res, next) => {
+        const { _id } = req.params
+        try {
+          const comic = await Chapter.findById(_id)
+          console.log(comic)
+          res.status(200).json({
+            success: true,
+            response: comic
+          })
+        }catch (error) {
+          next(error)
         }
-    }
+    },
 }
 
-export default controller
+export default controller;
