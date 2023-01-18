a245bd4d324cb5fe6d88d2f5469637e751202815
ElfanteCristianXVIII <79940633+Satch5918@users.noreply.github.com>
2023-01-18 11:16:14 -0300

    Companies 2 (#63)
    * models
    
    * controller
    
    * schema&validation
    
    * schema corrections
    
    * cambios back
    
    * controller&routes
    
    * controlador y querys funcionando
    
    * Fix (#58)
    
    * query con error
    
    * final changes
    
    Co-authored-by: Candela Cañete <candela.caniete1@gmail.com>
    Co-authored-by: candelacaniete <70610076+candelacaniete@users.noreply.github.com>

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,6 +1,7 @@
 import { Category } from '../models/Category.js'
+import { User } from '../models/User.js'
+import defaultResponse from '../config/response.js'
 
-import defaultResponse from "../config/response.js"
 
 const controller = {
     
@@ -17,9 +18,27 @@ const controller = {
     },
     
     read: async(req,res,next)=> {
+
+        //REQ ES UN OBJETO CON TOOOOOOODOS LOS REQUERIMIENTOS PARA PODER REALIZAR LA OPERACION
+        //REQ.BODY
+        //REQ.PARAMS
+        //REQ.QUERY
+        console.log(req.query) //para ver todas las consultas que vienen en la peticion
+        let consultasParaFiltrar = {} //se pasa adentro del metodo que busca
+       
         
-        try{
-            let all = await Category.find()
+        if (req.query.name) {
+            //consultasParaFiltrar.name = req.query.name.split(',') //para "cortar" un array de datos
+            consultasParaFiltrar.name = {"$regex": req.query.name, $options: 'i'} //expresion para incluir "palabras" a la busqueda
+        }
+        if (req.query.ranking) {
+            consultasParaFiltrar.ranking = Number(req.query.ranking)
+        }
+        
+       
+        try {
+            let all = await Category.find(consultasParaFiltrar)
+                        .sort({name: "asc"}
 
             if (all) {
                 req.body.success = true
