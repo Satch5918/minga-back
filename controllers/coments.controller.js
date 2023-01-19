4fd04542fe7661cd222fad903a598c6c60368d98
Candela Cañete <candela.caniete1@gmail.com>
2023-01-19 20:06:01 -0300

    controladores y rutas de put, delete y un  get listos

--- controllers/coments.controller.js
+++ controllers/coments.controller.js
@@ -1,8 +1,31 @@
+import { query } from 'express'
 import  {Comment}  from '../models/Comment.js'
 
 
 const controller = {
   create: async(req,res, next)=> { 
+
+    let query = {}
+    let order = {createdAt: "des"}
+    let pagination = {
+      limit: 5,
+      page: 1
+    }
+    if(req.query.page){
+      pagination.page = req.query.page
+    }
+    
+    if(req.query.limit){
+      pagination.limit = req.query.limit
+    }
+
+    if(req.query.chapter){
+      query = {
+        ... query,
+        commentable_type: req.query.chapter
+      }
+
+    }
       try {
           const { chapter_id, text,commentable_id } = req.body
           await Comment.create({ chapter_id, user_id: req.user.id ,text,commentable_id })
@@ -14,7 +37,57 @@ const controller = {
       } catch(error) {
           next(error)
       }
+  }, 
+  // get_comment_from_comment : async(req, res, next) => {
+  //   let query = {}
+  //   let order = {createdAt: "des"}
+  //   let pagination = {
+  //     limit: 3,
+  //     page: 1
+  //   }  
+
+  //   if(req.query.limit){
+  //     pagination.limit = req.query.limit
+  //   }
+
+  //   if(req.query.id){
+  //     query = {
+  //       ...query,
+  //       commentable_type: req.query.id
+  //     }
+  //   }
+
+  //   if(req.query.page){
+  //     pagination.page = req.query.page
+  //   }
+
+  // }, 
+  update_comment : async(req, res, next) =>{
+    try{
+      const {text,commentable_id, chapter_id} = req.body
+        await Comment.findOneAndUpdate({text, commentable_id, chapter_id});
+       res.status(201).json({
+        success: true,
+        response: 'done',
+    })
+    }catch(error){
+      next(error)
+    }
+  }, 
+  delete_comment: async(req,res,next) =>{
+    try{
+      const {text,commentable_id, chapter_id} = req.body
+        await Comment.remove({text, commentable_id, chapter_id});
+       res.status(201).json({
+        success: true,
+        response: 'done',
+    })
+    }catch(error){
+      next(error)
+    }
+
   }
+  
 }
 export default controller
 
--- routes/comments.route.js
+++ routes/comments.route.js
@@ -3,11 +3,13 @@ let router = express.Router();
 import schema from '../schemas/comment.schema.js'
 import validator from '../middlewares/validator.js'
 import controller from '../controllers/coments.controller.js'
-const { create } = controller
+const { create, update_comment , delete_comment} = controller
+
 import passport from "passport";
 import mustSignIn from '../middlewares/mustSignIn.js'
 
 /* GET users listing. */
 router.post('/',passport.authenticate('jwt',{session: false}), mustSignIn,validator(schema),create)
-
+router.put('/:id', validator(schema), update_comment)
+router.delete('/:id', delete_comment)
 export default router;
