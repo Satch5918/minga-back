0d222719beb84f01be7af1ffff79fbbf356384fe
valencaceres <vcaceres102@gmail.com>
2023-01-20 17:54:15 -0300

    ahora si funciona, y tiene los schemas configurados para editar cualquier campo

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,5 +1,5 @@
 import controller from "../controllers/author.controllers.js";
-import schema from "../schemas/authors.schema.js";
+import schemas from "../schemas/authors.schema.js";
 import  validator  from "../middlewares/validator.js";
 import express from 'express'
 import isAuthor from '../middlewares/isAuthor.js'
@@ -7,10 +7,11 @@ import passport from "passport";
 import authorEditController from "../controllers/author.edit.controller.js";
 let router = express.Router();
 
+const {schemaPost, schemaPut} = schemas
 const {update} = authorEditController
 const {create} = controller;
-router.post('/',passport.authenticate('jwt',{session: false}),validator(schema), create)
-router.put('/me', passport.authenticate('jwt',{session: false}), validator(schema), isAuthor ,update)
+router.post('/',passport.authenticate('jwt',{session: false}),validator(schemaPost), create)
+router.put('/me', passport.authenticate('jwt',{session: false}), validator(schemaPut), isAuthor ,update)
 
 
 export default router;
--- schemas/authors.schema.js
+++ schemas/authors.schema.js
@@ -1,6 +1,6 @@
 import Joi from 'joi-oid'
 
-const schema = Joi.object({
+const schemaPost = Joi.object({
    name:Joi.string().min(1).max(20).required().messages({
       "any.required":"Please enter your name",
       "string.empty":"Please enter your name"
@@ -28,5 +28,35 @@ const schema = Joi.object({
    }),
    active: Joi.boolean().required()
 })
+const schemaPut = Joi.object({
+   name: Joi.string().min(1).max(20).messages({
+      "any.required":"Please enter your name",
+      "string.empty":"Please enter your name"
+   }),
+   last_name: Joi.string().min(1).max(20).messages({
+      "any.required":"Please enter your last name",
+      "string.empty":"Please enter your last name"
+   }),
+   city:Joi.string().min(1).max(20).messages({
+      "any.required":"Please enter your city",
+      "string.required":"Please enter your city"
+   }),
+   country:Joi.string().min(1).max(20).messages({
+      "any.required":"Please enter your country",
+      "string.empty":"Please enter your country"
+   }),
+   date: Joi.date().messages({
+      "any.required":"Your date is empty, please enter your date",
+      "number.empty":"Your date is empty, please enter your date"
+   }),
+   photo:Joi.string().uri().messages({
+      "any.required":"Please enter your photo",
+      "string.required":"Please enter your photo",
+      "string.uri":"This field required an url, please enter an url with your photo"
+   }),
+   active: Joi.boolean()
+})
 
-export default schema
+const schemas = {schemaPost, schemaPut}
+
+export default schemas
