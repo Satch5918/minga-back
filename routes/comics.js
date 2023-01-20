c35c10e8d3ba26588d06fe041d87109a61786a5d
Satch5918 <satch_elfante@hotmail.com>
2023-01-20 04:04:13 -0300

    Controlador + Rutes

--- routes/comics.js
+++ routes/comics.js
@@ -5,30 +5,24 @@ import tittleExist from '../middlewares/tittleExist.js'
 import controllerDetails from '../controllers/comics.id.controller.js';
 import controllerCompany from '../controllers/comic.from.company.controller.js'
 import passport from "passport";
-
-const {get_comics_from_cia} = controllerCompany
-
-
-const { create } = controller
-import express from 'express';
-let router = express.Router();
-
-
-const {get_comic} = controllerDetails
-
-
-
 import all from "../controllers/comicall.controller.js"
 import isAuthor from '../middlewares/isAuthor.js';
 import authorIsActive from '../middlewares/authorIsActive.js';
+import express from 'express';
 
+const {get_comics_from_cia} = controllerCompany
+const { create, update, destroy } = controller
+
+let router = express.Router();
+const {get_comic} = controllerDetails
 const { read } = all
-
 
 router.get('/',read) // modificar nombre metodo 
 router.post('/',passport.authenticate('jwt',{session: false}), isAuthor, authorIsActive,validator(schema),tittleExist,create);
 router.get('/profile/company', get_comics_from_cia)
 router.get('/:id', get_comic)
+router.put('/comics/:id',update)
+router.delete('/comics/:id',destroy)
 
 
 export default router;
