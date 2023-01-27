4c82acb3daa121363199bf093d1d38dac69ffa4f
ElfanteCristianXVIII <79940633+Satch5918@users.noreply.github.com>
2023-01-27 09:28:55 -0300

    Sub comic4 (#76)
    * Controlador + Rutes
    
    * corregi Controlado, se agrego Middleware
    
    * correccion de todo
    
    * TERMINADO BACK
    
    * finish back
    
    * READY TO F
    
    * Listo

--- schemas/comic.schema.js
+++ schemas/comic.schema.js
@@ -26,8 +26,22 @@ export const schema = Joi.object({
 })
 
 export const comicSchema = Joi.object({
-    title: Joi.string().min(1).required().messages({
+    title: Joi.string().min(1).messages({
         'any.required': 'please put title of manga or comic',
         'string.empty': 'please put title of manga or comic'
-})
+}),
+photo: Joi.string().min(1).uri().messages({
+    'any.required': 'please put an url image ',
+    'string.empty': 'please put an url image ',
+    'string.uri': 'please put an url'
+}),
+description: Joi.string().min(1).messages({
+    'any.required': 'please put description of manga or comic',
+    'string.empty': 'please put description of manga or comic'
+}),
+category: Joi.objectId().messages({
+    'any.required': 'please put category id',
+    'string.empty': 'please put category id'
+}) 
+
 })
