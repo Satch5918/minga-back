3bef6cfd0554636542d2b3dc7ac86edcd33d59b2
Satch5918 <satch_elfante@hotmail.com>
2023-01-27 10:57:31 -0300

    Primer Push

--- nota.js
+++ nota.js
@@ -1,6 +1,6 @@
 /* 
 "useSelector" : es un hook de React-Redux que permite a un componente acceder al estado almacenado en un Store de Redux. Se utiliza para seleccionar una pieza específica de estado del Store.
-
+npm run
 "useDispatch" es un hook de React-Redux que proporciona una función dispatch para enviar acciones al store de Redux. Es una forma de actualizar el estado de la aplicación a través de las acciones de Redux.
 La función dispatch toma una acción como argumento y la envía al almacén de Redux para actualizar el estado.
 
@@ -11,6 +11,8 @@ La función dispatch toma una acción como argumento y la envía al almacén de 
 En Mongoose "Query" es un objeto que se utiliza para realizar operaciones de consulta en una colección de MongoDB. Puede utilizar una Query para recuperar documentos de una colección, actualizarlos, eliminarlos, contarlos, ordenarlos etc.
 
 En Mongoose, "params" se refiere a los parámetros que se pasan a una consulta o método para especificar los detalles de la consulta. Los parámetros son opcionales y pueden ser utilizados para especificar condiciones de búsqueda, campos de proyección, opciones de paginación, ordenamiento, etc.
+
+popular datos --> encontrar datos qeu tenemos guardado en la referencia de mongo, transformo los object id de mongo por los datos
 
 */
 
