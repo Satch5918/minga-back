1075edfc629fe7e24f4c8ef09c1fc114991c4c27
ElfanteCristianXVIII <79940633+Satch5918@users.noreply.github.com>
2023-02-02 17:31:54 -0300

    Rama user2 (#81)
    * 29/01
    
    * no anda nada
    
    * sigue sin funcionar
    
    * funcionando metodo PUT
    
    * finalizado los controladores
    
    * asd
    
    * TERMINADO

---
+++ routes/admin.router.js
@@ -0,0 +1,19 @@
+import express  from "express";
+import passport from "passport";
+import adminController from "../controllers/admin.controllers.js";
+import validator from '../middlewares/validator.js'
+import isAdmin from '../middlewares/isAdmin.js'
+
+
+const {updateRoleCompany, updateRoleAuthor } = adminController
+
+
+let router = express.Router()
+
+
+router.put('/company/:id',passport.authenticate('jwt',{session: false}),isAdmin, updateRoleCompany)
+router.put('/author/:id',passport.authenticate('jwt',{session: false}),isAdmin, updateRoleAuthor)
+
+
+
+export default router
--- routes/companies.route.js
+++ routes/companies.route.js
@@ -6,14 +6,18 @@ import passport from "passport";
 import mustSignIn from "../middlewares/mustSignIn.js";
 import companyEditController from "../controllers/company.edit.controller.js";
 import isCompany from "../middlewares/isCompany.js";
+import adminController from "../controllers/admin.controllers.js";
 
 const {schemaPost, schemaPut} = schemas
 const {update} = companyEditController
 const { create, get_company } = controller
 let router = express.Router()
+const {getCompanies} = adminController
 
+router.get('/',passport.authenticate('jwt',{session: false}),getCompanies)
 router.post('/',passport.authenticate('jwt',{session: false}), mustSignIn ,validator(schemaPost), create)
 router.get('/:id', get_company)// el nombre del params tiene que ser el mismo en ruta y en controlador 
 router.put('/:me',passport.authenticate('jwt',{session: false}), validator(schemaPut), isCompany,update)
 // ruta provicional
+
 export default router
